84e2c2e687e2243b512f1ed8a9b730ced1810f77
Safi100 <safinafi12@gmail.com>
2022-10-20 16:47:30 +0300

    add ejsMate

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const morgan = require('morgan')
 const methodOverRide = require('method-override')
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -11,11 +12,13 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const app = express()
 
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverRide('_method'))
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res)=> {
     res.render("home")
