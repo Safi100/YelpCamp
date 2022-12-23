5cf5b2a7a4fdcdeabaf7587a1017a425a9716f74
Safi Nafi <safinafi12@gmail.com>
2022-12-24 01:36:21 +0200

    add reviews array  to campground model

--- models/campground.js
+++ models/campground.js
@@ -2,26 +2,17 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const CampgroundSchema = new Schema({
-    title:{
-        type: String,
-        // required: true
-    },
-    image:{
-        type: String,
-        // required: true
-    },
-    price:{
-        type: Number,
-        // required: true
-    },
-    description:{
-        type: String,
-        // required: true
-    },
-    location:{
-        type: String,
-        // required: true
-    }
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    reviews : [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
