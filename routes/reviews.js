a7f486e5a84bacb3d355ec9f53072905df0e002c
Safi Nafi <safinafi12@gmail.com>
2022-12-26 18:12:52 +0200

    make reviews router

---
+++ routes/reviews.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const Router = express.Router({ mergeParams: true })
+const catchAsync = require('../utils/catchAsync')
+const ExpressError = require('../utils/ExpressError')
+const {reviewSchema} = require('../schemas')
+const Campground = require('../models/campground')
+const Review = require('../models/review')
+
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }else{
+        next()
+    }
+}
+Router.post('/', validateReview, catchAsync(async (req, res) => {
+    const id = req.params.id
+    const campground = await Campground.findById(id)
+    const review = new Review(req.body.review)
+    campground.reviews.push(review)
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+Router.delete('/:reviewId', catchAsync(async (req, res) => {
+    const {id, reviewId} = req.params
+    await Campground.findByIdAndUpdate(id, {$pull: { reviews: reviewId }})
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`)
+}))
+
+module.exports = Router
