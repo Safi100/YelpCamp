7fcdb9ccc8c476043236fee48605ed0829032f2e
Safi100 <safinafi12@gmail.com>
2023-01-24 13:27:00 +0200

    create autenticated middleware

---
+++ middleware.js
@@ -0,0 +1,9 @@
+const isLoggedIn = (req, res, next) => {
+    if(!req.isAuthenticated()){
+        req.flash('error', 'You must be signed in first!')
+        return res.redirect('/login')
+    }
+    next()
+}
+
+module.exports = isLoggedIn
