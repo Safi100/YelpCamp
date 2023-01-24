785c375d0c582e01e7bd6db15cb8e497912dce5a
Safi100 <safinafi12@gmail.com>
2023-01-24 13:19:11 +0200

    make login form with request

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
+const passport = require('passport')
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -17,5 +18,12 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('/register')
     }
 }))
+router.get('/login', (req, res) => {
+    res.render("users/login")
+})
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'} ), (req, res) => {
+    req.flash('success', 'welcome back!')
+    res.redirect('/campgrounds')
+})
 
 module.exports = router
