947731e3dbbbff5bbec4fadb30764e9b5e3608b1
Safi100 <safinafi12@gmail.com>
2023-01-24 16:39:57 +0200

    auto login after register

--- routes/user.js
+++ routes/user.js
@@ -7,12 +7,15 @@ const passport = require('passport')
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try{
         const {email, username, password} = req.body
         const user = new User({email, username})
         const registeredUser = await User.register(user, password)
-        res.redirect('/campgrounds')
+        req.login(registeredUser, err => {
+            if(err) return next(err)
+            res.redirect('/campgrounds')
+        })
     }catch(e){
         req.flash('error', e.message)
         res.redirect('/register')
