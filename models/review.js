4f682d34ef079f913ca1fe99ac751a210f6b3e2d
Safi100 <safinafi12@gmail.com>
2023-01-26 03:14:04 +0200

    add author to review

--- models/review.js
+++ models/review.js
@@ -4,5 +4,9 @@ const Schema = mongoose.Schema
 const reviewSchema = new Schema({
     body:String,
     rating:Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 module.exports = mongoose.model('Review', reviewSchema)
