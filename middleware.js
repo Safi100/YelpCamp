4773ad7a12d72852fa63aaca6493687710df2efa
Safi100 <safinafi12@gmail.com>
2023-01-26 02:52:34 +0200

    isAuthor move to middleware file

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+const Campground = require("./models/campground")
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()){
         req.session.returnTo = req.originalUrl
@@ -10,6 +12,20 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.checkReturnTo = (req, res, next) => {
     if(req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo
+    }
+    next()
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const id = req.params.id
+    const campground = await Campground.findById(id)
+    if(!campground){
+        req.flash('error', 'Cannot find that campground!')
+        return res.redirect('/campgrounds')
+    }
+    if(!campground.author.equals(req.user._id)){
+        req.flash('error', "You don't have permission to do that!")
+        return res.redirect(`/campgrounds/${id}`)
     }
     next()
 }
