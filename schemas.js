6c807c14ed2cba0a131f52996d12f18a7b897df5
Safi100 <safinafi12@gmail.com>
2023-01-27 22:57:27 +0200

    remove image joi validate for now

--- schemas.js
+++ schemas.js
@@ -4,7 +4,7 @@ module.exports.campgroundSchema = joi.object({
     campground: joi.object({
         title: joi.string().required(),
         price: joi.number().required().min(0),
-        image: joi.string().required(),
+        // image: joi.string().required(),
         location: joi.string().required(),
         description: joi.string().required()
     }).required()
