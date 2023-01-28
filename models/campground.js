d2b217be06538161b70f7b3468689c1a31e88767
Safi100 <safinafi12@gmail.com>
2023-01-28 16:35:22 +0200

    Adding a Thumbnail Virtual Property

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Review = require('./review')
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200')
+})
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
