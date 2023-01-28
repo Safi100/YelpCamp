df1a6bfea7b8f676d5ae2076b2d05ae22eb9a2e4
Safi100 <safinafi12@gmail.com>
2023-01-28 15:17:19 +0200

    create upload images in put request

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -32,6 +32,9 @@ module.exports.renderEditForm = async (req, res)=> {
 module.exports.updateCampground = async (req, res)=> {
     const id = req.params.id
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    const imgs = req.files.map(file => ({url: file.path, filename: file.filename}))
+    campground.images.push(...imgs)
+    await campground.save()
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,7 +15,7 @@ Router.route('/')
 
 Router.route('/:id') 
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 Router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
