f9a1bd70bcf6e51667dc2b98f749bf48b423c579
Safi100 <safinafi12@gmail.com>
2023-01-29 17:23:25 +0200

    add geomtry to post request

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,9 @@
 const Campground = require("../models/campground")
-const { cloudinary } = require('../cloudinary') 
+const { cloudinary } = require('../cloudinary')
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+const mapBoxToken = process.env.MAPBOX_TOKEN
+const geocoder = mbxGeocoding({accessToken:mapBoxToken})
+
 module.exports.index = async (req, res)=> {
     const campgrounds = await Campground.find({})
     res.render("campgrounds/index", {campgrounds})
@@ -17,7 +21,12 @@ module.exports.showCampground = async (req, res)=> {
     res.render('campgrounds/show', {campground})
 }
 module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
     const campground = new Campground(req.body.campground)
+    campground.geometry = geoData.body.features[0].geometry
     campground.images = req.files.map(file => ({url: file.path, filename: file.filename}))
     campground.author = req.user._id
     await campground.save()
