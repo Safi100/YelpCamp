ab18ebd29f1e4619b986c4118a6f0b99648cbe0e
Safi100 <safinafi12@gmail.com>
2023-01-29 19:05:41 +0200

    add map marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -6,3 +6,6 @@ const map = new mapboxgl.Map({
     center: [-74.5, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+new mapboxgl.Marker()
+.setLngLat([-74.5, 40])
+.addTo(map)
