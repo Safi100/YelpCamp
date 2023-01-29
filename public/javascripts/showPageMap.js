5dcc0a56c10a0fdcb50659f81dcb43d8eee17a9a
Safi100 <safinafi12@gmail.com>
2023-01-29 21:02:53 +0200

    add popup with campground name to marker map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,9 +3,15 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: campgroundGeomtry.coordinates, // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 10 // starting zoom
 });
 new mapboxgl.Marker()
-.setLngLat(campgroundGeomtry.coordinates)
+.setLngLat(campground.geometry.coordinates)
+.setPopup(
+    new mapboxgl.Popup({offset: 25})
+    .setHTML(
+        `<h4 class="mb-0">${campground.title}</h4>`
+    )
+)
 .addTo(map)
