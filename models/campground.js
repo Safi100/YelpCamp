145b81f978ade7890e277179bdbe0acc15ca9ab0
Safi100 <safinafi12@gmail.com>
2023-01-30 01:37:55 +0200

    create virtual campground for popup property

--- models/campground.js
+++ models/campground.js
@@ -10,7 +10,7 @@ ImageSchema.virtual('thumbnail').get(function() {
     return this.url.replace('/upload', '/upload/w_200')
 })
 
-const opts = { toJSON: { virtuals: true } };
+const opts = { toJSON: { virtuals: true } }
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -45,7 +45,7 @@ CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
     return `
     <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
     <p>${this.description.substring(0, 20)}...</p>`
-});
+})
 
 CampgroundSchema.post('findOneAndDelete', async function(doc) {
     if(doc){
