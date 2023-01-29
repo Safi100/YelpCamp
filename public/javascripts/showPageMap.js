0fb6c627f556172e7c31ab1b1087317f1d489903
Safi100 <safinafi12@gmail.com>
2023-01-29 18:23:30 +0200

    move map script to public scripts

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = mapToken;
+  
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
