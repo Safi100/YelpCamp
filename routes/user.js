35b6f81634c749e13d2e5cbdf00407fb082d395d
Safi100 <safinafi12@gmail.com>
2023-01-31 23:14:20 +0200

    create delete request for delete account

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
 const catchAsync = require('../utils/catchAsync')
-const { checkReturnTo} = require('../middleware')
+const { checkReturnTo, isAccountOwner, isLoggedIn } = require('../middleware')
 const passport = require('passport')
 const users = require('../controllers/users')
 
+router.route('/user/:id')
+.get(catchAsync(users.renderProfile))
+.delete(isLoggedIn ,isAccountOwner, catchAsync(users.deleteAccount))
+
 router.route('/register')
-    .get(users.renderRegister)
-    .post(catchAsync(users.register))
+.get(users.renderRegister)
+.post(catchAsync(users.register))
 
 router.route('/login')
-    .get(users.renderLogin)
-    .post(checkReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
-    
+.get(users.renderLogin)
+.post(checkReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
+
 router.get('/logout', users.logout)
+
 
 module.exports = router
