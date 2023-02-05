ed0a189f23c72526131362f8d6ae449df19d4712
Safi100 <safinafi12@gmail.com>
2023-02-05 16:04:50 +0200

    install and add sanitize-html for security

--- schemas.js
+++ schemas.js
@@ -1,10 +1,30 @@
 const joi = require('joi')
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                })
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+})
 
 module.exports.campgroundSchema = joi.object({
     campground: joi.object({
         title: joi.string().required(),
         price: joi.number().required().min(0),
-        // image: joi.string().required(),
         location: joi.string().required(),
         description: joi.string().required()
     }).required(),
